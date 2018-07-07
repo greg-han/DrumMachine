36e0ed09bcd21b93096fcae8fc385e36ec885f1a
Greg <24735588+greg-han@users.noreply.github.com>
2018-07-07 14:05:36 -0700

    keydown functionality works, will try to remove event liteners in favor of keydown but may just keep event listeners

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -10,22 +10,37 @@ const padStyle = {
   marginRight: '3px',
   marginTop: '3px',
   marginBottom: '3px',
-  borderRadius: '5px'
+  borderRadius: '5px',
+  textAlign: 'center'
 }
 
 class Pad extends Component {
   constructor(props){
     super(props);
   this.soundOff = this.soundOff.bind(this);
+  this.keySound = this.keySound.bind(this);
+  }
+  componentDidMount() {
+   document.addEventListener("keydown",this.keySound);
+  }
+  componentWillUnmount() {
+   document.removeEventListener("keydown",this.keySound);
+  }
+  keySound(event){
+   console.log(event.key);
+   const sound = document.getElementById(String(event.key).toUpperCase());
+   if(sound){
+     sound.play();
+    }  
   }
   soundOff(event){
-   console.log("I'm clicked");
+   console.log("I'm clicked",event);
    const sound = document.getElementById(this.props.keyTrigger);
    sound.play();
   } 
   render() { 
     return ( 
-      <div id={this.props.clipId} className="drum-pad" style={padStyle} onClick={this.soundOff}>
+      <div id={this.props.clipId} className="drum-pad" style={padStyle} onClick={this.soundOff} onKeyDown={this.keySound} >
         <audio className='clip' id={this.props.keyTrigger} src={this.props.clip}></audio>
       {this.props.keyTrigger}
       </div>    
